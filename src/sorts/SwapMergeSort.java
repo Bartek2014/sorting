360e1df9574eb2e3479949730400b6e1e1a83105
Gaming32 <soujournme@gmail.com>
2020-09-02 19:50:27 -0500

    Create SwapMergeSort.java

---
+++ src/sorts/SwapMergeSort.java
@@ -0,0 +1,114 @@
+package sorts;
+
+import templates.Sort;
+import utils.Delays;
+import utils.Highlights;
+import utils.Reads;
+import utils.Writes;
+
+/*
+ * 
+MIT License
+
+Copyright (c) 2019 w0rthy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ *
+ */
+
+final public class SwapMergeSort extends Sort {
+    private BinaryInsertionSort binaryInserter;
+
+    public SwapMergeSort(Delays delayOps, Highlights markOps, Reads readOps, Writes writeOps) {
+        super(delayOps, markOps, readOps, writeOps);
+        
+        this.setSortPromptID("SwapMerge");
+        this.setRunAllID("SwapMerge Sort");
+        this.setReportSortID("SwapMergesort");
+        this.setCategory("Hybrid Sorts");
+        this.isComparisonBased(true);
+        this.isBucketSort(false);
+        this.isRadixSort(false);
+        this.isUnreasonablySlow(false);
+        this.setUnreasonableLimit(0);
+        this.isBogoSort(false);
+    }
+
+    private void merge(int[] array, int leftStart, int rightStart, int end) {
+        int left = leftStart;
+        int right = rightStart;
+
+        while (left < right) {
+            if (left >= end || right >= end) {
+                break;
+            }
+            else if (Reads.compare(array[left], array[right]) <= 0) {
+                left += 1;
+            }
+            else {
+                // Call movedown
+                left += 1;
+                right += 1;
+            }
+        }
+
+        if (left >= end) {
+            return;
+        }
+        else if (Reads.compare(array[left-1], array[right-1]) > 0) {
+            Writes.swap(array, left-1, right-1, 0.075, true, false);
+        }
+        else if (Reads.compare(array[left], array[right]) > 0) {
+            Writes.swap(array, left, right, 0.075, true, false);
+        }
+    }
+
+    private void mergeRun(int[] array, int start, int mid, int end) {
+        if(start == mid) return;
+        
+        mergeRun(array, start, (mid+start)/2, mid);
+        mergeRun(array, mid, (mid+end)/2, end);
+
+        if(end - start < 32) {
+            return;
+        }
+        else if(end - start == 32) {
+            binaryInserter.customBinaryInsert(array, start, end, 0.333);
+        }
+        else {
+            merge(array, start, mid, end);
+        }
+    }
+    
+    @Override
+    public void runSort(int[] array, int length, int bucketCount) {
+        binaryInserter = new BinaryInsertionSort(this.Delays, this.Highlights, this.Reads, this.Writes);
+        
+        if(length < 32) {
+            binaryInserter.customBinaryInsert(array, 0, length, 0.333);
+            return;
+        }
+        
+        int start = 0;
+        int end = length;
+        int mid = start + ((end - start) / 2);
+        
+        mergeRun(array, start, mid, end);
+    }
+}
